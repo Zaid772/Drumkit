ddc85149de8df5921b5a59fad992b9b474a93fb0
Zaid <zedpx101@outlook.com>
2023-08-16 11:43:04 +0100

    keydown event

--- script.js
+++ script.js
@@ -10,13 +10,60 @@ const box9 = document.getElementById('box9');
 
 const boom = () => {
     let audio = new Audio("boom.wav");
-    audio.play()
+        audio.play();
 }
 box1.addEventListener('click', boom);
-
+document.addEventListener('keydown', function(event) {
+    if(event.key == 'a'){
+        boom();
+    }
+});
 const clap = () => {
     let audio = new Audio("clap.wav");
-    audio.play()
+    audio.play();
 }
 box2.addEventListener('click', clap);
+
+const hihat = () => {
+    let audio = new Audio("hihat.wav");
+    audio.play()
+}
+box3.addEventListener('click', hihat);
+
+const kick = () => {
+    let audio = new Audio("kick.wav");
+    audio.play()
+}
+box4.addEventListener('click', kick);
+
+const openhat = () => {
+    let audio = new Audio("openhat.wav");
+    audio.play()
+}
+box5.addEventListener('click', openhat);
+
+const ride = () => {
+    let audio = new Audio("ride.wav");
+    audio.play()
+}
+box6.addEventListener('click', ride);
+
+const snare = () => {
+    let audio = new Audio("snare.wav");
+    audio.play()
+}
+box7.addEventListener('click', snare);
+
+const tink = () => {
+    let audio = new Audio("tink.wav");
+    audio.play()
+}
+box8.addEventListener('click', tink);
+
+const tom = () => {
+    let audio = new Audio("tom.wav");
+    audio.play()
+}
+box9.addEventListener('click', tom);
+
 
