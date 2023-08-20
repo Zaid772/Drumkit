bc9ee584a26c9698481c983871661847d3380025
Zaid <zedpx101@outlook.com>
2023-08-20 16:53:02 +0100

    final

--- script.js
+++ script.js
@@ -13,57 +13,96 @@ const boom = () => {
         audio.play();
 }
 box1.addEventListener('click', boom);
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', (event) => {
     if(event.key == 'a'){
         boom();
     }
 });
+
 const clap = () => {
     let audio = new Audio("clap.wav");
     audio.play();
 }
 box2.addEventListener('click', clap);
+document.addEventListener('keydown', (event) => {
+    if(event.key == 's'){
+        clap();
+    }
+});
 
 const hihat = () => {
     let audio = new Audio("hihat.wav");
     audio.play()
 }
 box3.addEventListener('click', hihat);
+document.addEventListener('keydown', (event) => {
+    if(event.key == 'd'){
+        hihat();
+    }
+});
 
 const kick = () => {
     let audio = new Audio("kick.wav");
     audio.play()
 }
 box4.addEventListener('click', kick);
+document.addEventListener('keydown', (event) => {
+    if(event.key == 'f'){
+        kick();
+    }
+});
 
 const openhat = () => {
     let audio = new Audio("openhat.wav");
     audio.play()
 }
 box5.addEventListener('click', openhat);
+document.addEventListener('keydown', (event) => {
+    if(event.key == 'g'){
+        openhat();
+    }
+});
 
 const ride = () => {
     let audio = new Audio("ride.wav");
     audio.play()
 }
 box6.addEventListener('click', ride);
+document.addEventListener('keydown', (event) => {
+    if(event.key == 'h'){
+        ride();
+    }
+});
 
 const snare = () => {
     let audio = new Audio("snare.wav");
     audio.play()
 }
 box7.addEventListener('click', snare);
+document.addEventListener('keydown', (event) => {
+    if(event.key == 'j'){
+        snare();
+    }
+});
 
 const tink = () => {
     let audio = new Audio("tink.wav");
     audio.play()
 }
 box8.addEventListener('click', tink);
-
+document.addEventListener('keydown', (event) => {
+    if(event.key == 'k'){
+        tink();
+    }
+});
 const tom = () => {
     let audio = new Audio("tom.wav");
     audio.play()
 }
 box9.addEventListener('click', tom);
-
+document.addEventListener('keydown',(event) => {
+    if(event.key == 'l'){
+        tom();
+    }
+});
 
